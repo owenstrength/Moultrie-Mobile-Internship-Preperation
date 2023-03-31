f43b5a7e885c75042520016f023ae4082d706fc9
Owen Strength <Owenstrength@gmail.com>
2023-03-31 12:13:41 -0500

    helloWorld

---
+++ C# Intro/HelloWorld.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace HelloWorldApp
+{
+    class Hello
+    {
+        static void Main(string[] args)
+        {
+            string Name = "Owen";
+            Console.WriteLine("Hello " + Name);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C# Intro/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorldApp
+{
+    class Hello
+    {
+        static void Main(string[] args)
+        {
+            string Name = "Owen";
+            Console.WriteLine("Hello " + Name);
+        }
+    }
+}
